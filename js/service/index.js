a3627dd7f7465aa0685d587da7920d9137c68f5b
Krishna Tangirala <ktangirala@spglobal.com>
2021-03-30 20:01:45 +0530

    Added check for invalid URL

--- js/service/index.js
+++ js/service/index.js
@@ -9,13 +9,20 @@ application.listen(port, () => { console.log("Server running on port " + port)})
 
 // Test URL
 application.get("/urlshortener", (request, response, next) => {
+    var message = "";
+
     var params = url.parse(request.url, true).query;
     var paramurl = params["url"];
-    var message = "";
     if (!paramurl) {
         message = "No URL was entered. Try appending this request with a URL to shorten. E.g., <request_url>?url=https://google.com";
     } else {
-        message = "This is the backend service for URL shortener. It will accept any URL and return the shortened URL. Work in Progress!!, You have entered the url: " + paramurl;
+        try {
+            const URL = new url.URL(paramurl);
+            message = "This is the backend service for URL shortener. It will accept any URL and return the shortened URL. Work in Progress!!, You have entered the url: " + paramurl;
+        }
+        catch (error) {
+            message = "Invalid URL entered for shortening";
+        }
     }
 
     response.json(message);
